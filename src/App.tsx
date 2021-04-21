cd4c03cf2643211aeb3b4715f67f50c1f2efb3af
neillycubed <neillybusiness3@gmail.com>
2021-04-21 11:30:21 -0700

    add cart functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,70 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+import { Item } from './Item/Item';
+import { Cart } from './Cart/Cart';
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+import {Wrapper, StyledButton} from './App.styles';
+
+export type CartItemType = {
+    id: number;
+    category: string;
+    description: string;
+    image: string;
+    price: number;
+    title: string;
+    amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+    await (await fetch('https://fakestoreapi.com/products')).json();
+
 export const App = () => {
+    const [cartOpen, setCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([] as CartItemType[]);
+    const {data, isLoading, error} = useQuery<CartItemType[]>(
+        'products',
+        getProducts
+        );
+
+        console.log(data);
+
+        const getTotalItems = (items: CartItemType[]) => 
+            items.reduce((ack: number, item) => ack + item.amount, 0);
+
+        const handleAddToCart = (clickedItem: CartItemType) => null;
+
+        const handleRemoveFromCart = () => null;
+
+        if(isLoading) return <LinearProgress />;
+        if(error) return <div>Something went wrong!</div>
+
     return (
-        <h1></h1>
+        <Wrapper>
+            <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)} >
+                <Cart 
+                cartItems={cartItems}
+                addToCart={handleAddToCart}
+                removeFromCart={handleRemoveFromCart}
+                />    
+            </Drawer>
+            <StyledButton onClick={() => setCartOpen(true)}>
+                <Badge badgeContent={getTotalItems(cartItems)} color='error' >
+                    <AddShoppingCartIcon />
+                </Badge>
+            </StyledButton>
+            <Grid container spacing={3} >
+                {data?.map(item => (
+                    <Grid item key={item.id} xs={12} sm={4}>
+                        <Item item={item} handleAddToCart={handleAddToCart} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Wrapper>
     )
 }
